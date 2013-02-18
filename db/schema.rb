c6663d929ea22359f780e7a41e90a35934821769
JDStraughan <jdstraughan@gmail.com>
2013-02-17 19:15:24 -0600

    Adding sort_order for collectables

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215184746) do
+ActiveRecord::Schema.define(:version => 20130218011026) do
 
   create_table "collectables", :force => true do |t|
     t.integer  "user_id"
     t.string   "resource_uuid"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.integer  "sort_order"
   end
 
   create_table "users", :force => true do |t|
