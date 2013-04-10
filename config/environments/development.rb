499a5194fae18abcc3311d7677de21dd981d7c57
Jason Ellis <jason.ellis@yahoo.com>
2013-04-10 15:29:18 -0500

    Making config changes to search to point to right server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,6 @@ Browser::Application.configure do
   config.assets.debug = true
 
   # Elastic search server configs
-  config.elastic_search_url = 'http://esearch.agilix.com:9200/lr/lr/_search'
+  config.elastic_search_url = 'http://esearch.agilix.com:9200/lris/lrmi/_search'
 
 end
