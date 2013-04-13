3faf1967ad0fcb1db9e658f3c76ff7c7619050cf
JDStraughan <jdstraughan@gmail.com>
2013-04-13 14:08:40 -0500

    Removing files associated with users/collectables including unit tests, models, migrations, schema

--- db/migrate/20130215184746_create_collectables.rb
+++
@@ -1,9 +0,0 @@
-class CreateCollectables < ActiveRecord::Migration
-  def change
-    create_table :collectables do |t|
-      t.integer :user_id
-      t.string  :resource_uuid
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130218011026_add_sort_order_to_collectables.rb
+++
@@ -1,5 +0,0 @@
-class AddSortOrderToCollectables < ActiveRecord::Migration
-  def change
-    add_column :collectables, :sort_order, :integer
-  end
-end
