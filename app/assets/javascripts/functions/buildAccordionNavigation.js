bc562cf7c03eecc8ecbaa78328b02a572b408e49
Jason Ellis <jason.ellis@yahoo.com>
2013-05-08 13:23:41 -0500

    Refactoring accordion builder to use the object _order key order instead of its actual order since that can be random depending on the browser.

--- app/assets/javascripts/functions/buildAccordionNavigation.js
+++ app/assets/javascripts/functions/buildAccordionNavigation.js
@@ -14,7 +14,9 @@ function buildAccordionNavigation(div, req) {
       var title = accordionTitle(standard[i]._text);
       var links = "";
 
-      for (s in standard[i]._order) {
+      for (var s = 0; s < Object.keys(standard[i]._order).length; s++) {
+        if (standard[i]._order[s] == undefined) continue;
+
         var key = standard[i]._order[s].substr( standard[i]._order[s].lastIndexOf('.') + 1, standard[i]._order[s].length )
         var item = standard[i][key];
 
@@ -35,7 +37,9 @@ function buildAccordionNavigation(div, req) {
     var title = accordionTitle(standard._text);
     var links = "";
 
-    for (i in standard._order) {
+    for (var i = 0; i < Object.keys(standard._order).length; i++) {
+      if (standard._order[i] == undefined) continue;
+
       var linkText = standard._order[i].substr( standard._order[i].lastIndexOf(':') + 1, standard._order[i].length )
       var linkNotation = standard._order[i].substr( standard._order[i].lastIndexOf(':') + 1, standard._order[i].length )
       var key = standard._order[i].substr( standard._order[i].lastIndexOf('.') + 1, standard._order[i].length )
@@ -49,7 +53,9 @@ function buildAccordionNavigation(div, req) {
     // Inject the rest of the math standards
     var standard = jsonStandards.CCSS.Math.Content;
 
-    for (i in standard._order) {
+    for (var i = 0; i < Object.keys(standard._order).length; i++) {
+      if (standard._order[i] == undefined) continue;
+
       var key = standard._order[i].substr( standard._order[i].lastIndexOf('.') + 1, standard._order[i].length )
       var item = standard[key];
 
