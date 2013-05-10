bb709ab53fdb8b1970499d60b5851351e97d0bd5
Jason Ellis <jason.ellis@yahoo.com>
2013-05-10 09:59:28 -0500

    More fixes for IE8

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -473,6 +473,21 @@ $(function() {
     }
   });
 
+
+  $('[placeholder]').focus(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+    }
+  }).blur(function() {
+        var input = $(this);
+        if (input.val() == '' || input.val() == input.attr('placeholder')) {
+          input.addClass('placeholder');
+          input.val(input.attr('placeholder'));
+        }
+      }).blur();
+
 });
 
 // Set grade range and the slider
